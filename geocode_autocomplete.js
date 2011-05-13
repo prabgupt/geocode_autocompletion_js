7e3bbdd0de9de975ddd7e1febc5f9c8badfa68ab
Prabhat Gupta <itisme.prabhat@gmail.com>
2011-05-13 23:43:56 +0530

    added non-commercial licensing

--- geocode_autocomplete.js
+++ geocode_autocomplete.js
@@ -1,3 +1,21 @@
+/*
+
+Copyright (c) 2011 Prabhat Gupta(prabhatgupta.webs.com | golygon.com)
+
+This script may be used for non-commercial purposes only. For any
+commercial purposes, please contact the author at prabhat@golygon.com
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 var geocoder = new google.maps.Geocoder();
 
 $('.geocode_autocomplete').live('focus', function(){
