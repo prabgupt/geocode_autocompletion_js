683cd220b4dde992c99db798276be4c91faa47d5
Prabhat Gupta <itisme.prabhat@gmail.com>
2011-05-13 23:32:07 +0530

    showing provided location on map now rather displaying its lat/lng in text

--- geocode_autocomplete.js
+++ geocode_autocomplete.js
@@ -4,7 +4,7 @@ $('.geocode_autocomplete').live('focus', function(){
         $(this).autocomplete({
             source: function(request, response) {
               geocoder.geocode( {'address': request.term}, function(results, status) {
-                $("#map_location").attr("value","");
+                //$("#map_location").attr("value","");
                 response($.map(results, function(item) {
                     value = item.address_components[0].long_name;
                   return {
@@ -18,7 +18,25 @@ $('.geocode_autocomplete').live('focus', function(){
             },
             minLength: 2,
             select: function(event, ui) {
-               $("#map_location").html("Latitude: " + ui.item.location.lat() + " Longitude: "+ ui.item.location.lng());
+              // $("#map_location").html("Latitude: " + ui.item.location.lat() + " Longitude: "+ ui.item.location.lng());
+	      showMap(ui.item.location.lat(), ui.item.location.lng(), 8, 'map_location');
 	    }
           });
         });
+	
+function showMap(lat, lng, zoom, div) {
+  var location = new google.maps.LatLng(lat,lng);
+  var mapOptions = {
+    zoom: zoom,
+    center: location,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: true
+  };
+  var mapShow =  new google.maps.Map(document.getElementById(div), mapOptions);
+  var local_marker = new google.maps.Marker({
+	                position: location,
+	                animation: google.maps.Animation.DROP,
+	                map: mapShow
+               	     });
+}
+
